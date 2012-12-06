204b43b56e86082b3ba0482fdeef85ca1ceea379
Sudev <sudev@sudev.(none)>
2012-12-06 23:58:28 +0530

    recursive

--- C-Intro/fact.c
+++ C-Intro/fact.c
@@ -2,18 +2,19 @@
 int fact(int);
 void main()
 {
-printf("Numer ?");
-int a;
-scanf("%d",&a);
-printf("\nFactorial of number is %d\n",fact(a));
+  printf("Numer ?");
+  int a;
+  scanf("%d",&a);
+  printf("\nFactorial of number is %d\n",fact(a));
 }
+
 int fact(int i)
 {
-int m =1;
-printf("\n In fact i=%d m=%d", i,m);
-if (i > 1)
-m = i*fact(i-1);
-printf("\nout Fact i=%d m=%d",i,m);
-return(m);
+  int m =1;
+  printf("\n In fact i=%d m=%d", i,m);
+  if (i > 1)
+  m = i*fact(i-1);
+  printf("\nout Fact i=%d m=%d",i,m);
+  return(m);
 }
 
