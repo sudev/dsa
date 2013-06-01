8c55fb06f34a75e6111c1258b11195647626d8f5
sudev <sudevdev@gmail.com>
2013-06-01 12:42:56 +0530

    string

--- string matching/brute.c
+++ string matching/brute.c
@@ -7,24 +7,23 @@ int main(){
     fgets(str,256,stdin);
     printf("\nEnter the pattern that you want to match:\n");
     fgets(pat,256,stdin);
-//    newline(str);
-    if(brute(str,pat))
+    if(brute(str,pat)> 0)
     printf("Is a substring");
     else
-    printf("Not a substring");
+    printf("Not a substring%d",brute(str,pat));
     return 0;
 }
 
 int brute(char * str,char * pat){
     int i;
-    printf("len str %d \t pat %d",strlen(str),strlen(pat));
     for(i=0; i <= (strlen(str) - strlen(pat)); i++)
     {
         int j = 0;
         while( (j  < (strlen(pat) -1)) &&( pat[j] == str[j+i]))
-        {    
-            if(j == strlen(pat))
-                return i;
+        {   
+            printf("\nmathcing at %d\t%d times\n",i,j);
+            if(j == (strlen(pat)-2))
+                return (i+1);
             j++;
         }
     }
