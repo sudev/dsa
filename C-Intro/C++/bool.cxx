fbe6cd22a35980c500614394f6514cc7912275f2
sudev <sudevdev@gmail.com>
2013-06-23 01:15:41 +0530

    queue oop

--- C-Intro/C++/bool.cxx
+++ C-Intro/C++/bool.cxx
@@ -1,13 +1,19 @@
 #include <iostream>
 using namespace std;
 
+bool issingle(int a){
+return (a > 0 && a < 10);
+}
+
 
 // Bool can be assigned a conditional operation 
 int main(){
     int x;
     cin>>x;
     bool s = ( x > 1);
-    cout<<s;
+    cout<<s<<endl<<"Enter a Number to check if its single digit";
+    cin>>x;
+    cout << issingle(x)<<endl;
     return 0;
 }
 
